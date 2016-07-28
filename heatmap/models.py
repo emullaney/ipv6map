e17fcdd1232fa991333af59a01611f184276569e
Erin Mullaney <emullaney@caktusgroup.com>
2016-07-28 10:13:32 -0400

    Adding an index for faster searching by lat/long

--- heatmap/models.py
+++ heatmap/models.py
@@ -5,8 +5,8 @@ class ipv6Address(models.Model):
     network = models.CharField(max_length=255, blank=True)
     geoname_id = models.CharField(max_length=255, blank=True)
     postal_code = models.CharField(max_length=255, blank=True)
-    latitude = models.FloatField(null=True, blank=True)
-    longitude = models.FloatField(null=True, blank=True)
+    latitude = models.FloatField(db_index=True, null=True, blank=True)
+    longitude = models.FloatField(db_index=True, null=True, blank=True)
 
     class Meta:
         ordering = ('latitude', 'longitude', )
